e75274bf9a9f2265b88c6fc1bdaa9c98e9772bbb
Sera <a-nes.rnd@mail.ru>
2023-04-28 14:33:40 +0300

    Change database and change bot.py translate comments into russian

--- bot/bot.py
+++ bot/bot.py
@@ -7,117 +7,139 @@ from config import TOKEN
 from command_buttons import client_keyboard, technic_keyboard
 
 
-# creating a bot
+# Создание бота
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
 
+'''
+------------------------------------------------------------
+ФУНКЦИЯ СТАРТА БОТА И ОПРЕДЕЛЕНИЯ КЕМ ЯВЛЯЕТСЯ ПОЛЬЗОВАТЕЛЬ
+------------------------------------------------------------
+'''
+
+
 @dp.message_handler(commands=['start'])
 async def start_handler(message: types.Message):
-    # user definition
+    # Определение пользователя
     user_id = message.from_user.id
-    # checking if the user is a technician
-    # here you can use your own logic to define
+    # Проверка, является ли пользователь тех. специалистом
     is_technic = False
-    # adding a user to the database if it is not already there
+    # Добавление пользователя в базу данных, если его там еще нет
     db.add_user(user_id=user_id, username=message.from_user.username, user_status='technic' if is_technic else 'regular')
-    # send message to user
+    # Отправка сообщения пользователю
     if is_technic:
-        # processor for technical specialist
-        await message.answer('Добро пожаловать, тех.специалист! Выберите действие:', reply_markup=technic_keyboard)
+        # Обработчик для тех. специалиста
+        await message.answer(f'Добро пожаловать, {message.from_user.username}, вы вошли в режим работника тех.поддержки'
+                             f'! Выберите действие:', reply_markup=technic_keyboard)
     else:
-        # client handler
-        await message.answer('Добро пожаловать, клиент! Выберите действие:', reply_markup=client_keyboard)
+        # Обработчик для тех. специалиста
+        await message.answer(f'Добро пожаловать, {message.from_user.username}! Выберите действие:', reply_markup=client_keyboard)
 
 
-# handler for the "List of tickets" button on the client
-@dp.message_handler(text='Список тикетов', user_id=USER_ID, user_status='regular')
+'''
+---------------------
+ФУНКЦИИ ДЛЯ КЛИЕНТОВ
+---------------------
+'''
+
+
+# Обработчик для кнопки "Список тикетов" у клиента
+@dp.message_handler(text='Список тикетов', user_status='regular')
 async def show_ticket_list_handler(message: types.Message):
-    # getting a list of tickets from the database
-    ticket_list = db.get_ticket_list(user_id=message.from_user.id)
-    # sending a message to a user with a list of tickets
+    # Получение списка тикетов из базы данных
+    ticket_list = db.get_ticket_list_regular(user_id=message.from_user.id)
+    # Отправка сообщения пользователю со списком тикетов
     await message.answer('\n'.join(ticket_list))
 
 
-# handler for the "New ticket" button on the client
-@dp.message_handler(text='Новый тикет', user_id=USER_ID, user_status='regular')
+# Обработчик для кнопки "Новый тикет" у клиента
+@dp.message_handler(text='Новый тикет', user_status='regular')
 async def new_ticket_handler(message: types.Message):
-    # send a message to the user asking for the subject of the ticket
+    # Отправка сообщения пользователю с запросом на тему тикета
     await message.answer('Введите тему тикета:')
-    # move to the next handler
+    # Переход к следующему обработчику
     dp.register_message_handler(new_ticket_priority_handler, user_id=message.from_user.id, user_status='regular')
 
 
-# handler for the priority of the ticket from the client
+# Обработчик для приоритета тикета у клиента
 async def new_ticket_priority_handler(message: types.Message):
-    # get selected ticket priority
+    # Получение выбранного приоритета тикета
     priority = message.text
-    # sending a message to the user asking for a description of the problem
+    # Отправка сообщения пользователю с запросом на описание проблемы
     await message.answer('Опишите проблему:')
-    # adding a ticket to the database
+    # Добавление тикета в базу данных
     db.add_ticket(user=message.from_user.id, theme=1, message_text=message.text, false_priority=priority)
-    # move to the next handler
+    # Переход к следующему обработчику
     dp.register_message_handler(new_ticket_finish_handler, user_id=message.from_user.id, user_status='regular')
 
 
-# handler to complete the creation of a new ticket at the client
+# Обработчик для завершения создания нового тикета у клиента
 async def new_ticket_finish_handler(message: types.Message):
-    # sending a message to the user about the completion of the creation of the ticket
+    # Отправка сообщения пользователю о завершении создания тикета
     await message.answer('Тикет успешно создан! Ожидайте ответа.')
-    # jump to start handler
+    # Переход к начальному обработчику
     dp.register_message_handler(start_handler, commands=['start'])
 
 
-# handler for the "List of tickets" button for a technical specialist
-@dp.message_handler(text='Список тикетов', userid=USER_ID, userstatus='technic')
+'''
+----------------------------
+ФУНКЦИИ ДЛЯ ТЕХ.СПЕЦИАЛИСТОВ
+----------------------------
+'''
+
+
+# Обработчик для кнопки "Список тикетов" у тех. специалиста
+@dp.message_handler(text='Список тикетов', user_status='technic')
 async def show_ticket_list_handler(message: types.Message):
-    # getting a list of tickets from the database
-    ticket_list = db.get_ticket_list()
-    # sending a message to a user with a list of tickets
+    # получение списка тикетов из базы данных
+    ticket_list = db.get_ticket_list_technic()
+    # отправка сообщения пользователю со списком тикетов
     await message.answer('\n'.join(ticket_list))
 
 
-# handler for the "Select ticket" button from a technical specialist
-@dp.message_handler(text='Выбрать тикет', userid=USER_ID, userstatus='technic')
+# Обработчик для кнопки "Выбрать тикет" у тех. специалиста
+@dp.message_handler(text='Выбрать тикет', userstatus='technic')
 async def choose_ticket_handler(message: types.Message):
-    # sending a message to the user asking them to select a ticket
+    # отправка сообщения пользователю с запросом на выбор тикета
     await message.answer('Выберите тикет:')
-    # move to the next handler
-    dp.register_message_handler(technic_ticket_info_handler, userid=message.from_user.id, userstatus='technic')
+    # переход к следующему обработчику
+    dp.register_message_handler(technic_ticket_info_handler, user_id=message.from_user.id, user_status='technic')
 
 
-# handler for information about a ticket from a technical specialist
+# Обработчик для информации о тикете у тех. специалиста
 async def technic_ticket_info_handler(message: types.Message):
-    # getting information about a ticket from the database
-    ticket_info = db.get_ticket_info(ticketid=message.text)
-    # sending a message to the user with information about the ticket
+    # Получение информации о тикете из базы данных
+    ticket_info = db.get_ticket_info_technic(ticket_id=message.text)
+    # Отправка сообщения пользователю с информацией о тикете
     await message.answer(ticket_info)
-    # adding a button "Start session"
+    # Добавление кнопки "Начать сессию"
     keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
     keyboard.add(types.KeyboardButton('Начать сессию'))
     await message.answer('Нажмите кнопку "Начать сессию", чтобы начать общение с клиентом.', reply_markup=keyboard)
-    # move to the next handler
-    dp.register_message_handler(technic_ticket_info_handler, userid=message.from_user.id, userstatus='technic')
+    # Переход к следующему обработчику
+    dp.register_message_handler(technic_ticket_info_handler, user_id=message.from_user.id, user_status='technic')
 
 
-# handler for the session at the tech.specialist
+# Обработчик для сессии у тех. специалиста
 async def technic_session_handler(message: types.Message):
-    # sending a message to a client via a bot
+    # Отправка сообщения клиенту через-бота
     await bot.send_message(chat_id=message.text, text='Вы начали сессию с тех.специалистом! Можете задавать вопросы.')
-    # adding "End Session" button
+    # Добавление кнопки "Закончить сессию"
     keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
     keyboard.add(types.KeyboardButton('Закончить сессию'))
     await message.answer('Нажмите кнопку "Закончить сессию", чтобы завершить общение.', reply_markup=keyboard)
-    # move to the next handler
-    dp.register_message_handler(technic_feedback_handler, userid=message.from_user.id, userstatus='technic')
+    # Переход к следующему обработчику
+    dp.register_message_handler(technic_feedback_handler, user_id=message.from_user.id, user_status='technic')
 
 
-# processor for feedback on the work of a technical specialist at the client
+# Обработчик для отзыва о работе тех. специалиста у клиента
 async def technic_feedback_handler(message: types.Message):
-    # sending a message to the client asking for feedback
+    # Отправка сообщения клиенту с просьбой об оставлении отзыва
     await bot.send_message(chat_id=message.text, text='Оцените работу тех.специалиста!')
-    # jump to start handler
+    # Переход к начальному обработчику
     dp.register_message_handler(start_handler, commands=['start'])
+
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
--- bot/command_buttons.py
+++ bot/command_buttons.py
@@ -1,12 +1,12 @@
 from aiogram import types
 
-# client keyboard
+# Клиентская клавиатура
 client_ticket_list_button = types.KeyboardButton('Список тикетов')
 client_new_ticket_button = types.KeyboardButton('Новый тикет')
 client_keyboard = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
 client_keyboard.add(client_ticket_list_button, client_new_ticket_button)
 
-# keyboard for technician
+# Клавиатура для тех. специалистов
 technic_ticket_list_button = types.KeyboardButton('Список тикетов')
 technic_choose_ticket_button = types.KeyboardButton('Выбрать тикет')
 technic_keyboard = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
